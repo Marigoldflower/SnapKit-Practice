7c8f9de1b311022d7f2d2051fc5e1f0206de2b46
황홍필 <marigold410@naver.com>
2023-05-17 15:50:51 +0900

    SnapKit Commit
    스냅킷 연습 후 커밋

--- SnapKit_Practice/ViewController.swift
+++ SnapKit_Practice/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let yellowView: UIView = {
+        let yellow = UIView()
+        yellow.backgroundColor = .yellow
+        return yellow
+    }()
+    
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "매스버거"
+        return label
+    }()
+    
+    
+    let orangeView: UIView = {
+        let orange = UIView()
+        orange.backgroundColor = .orange
+        return orange
+    }()
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(yellowView)
+        view.addSubview(orangeView)
+        view.addSubview(label)
+        setupLayout()
+        
     }
 
+    
+    func setupLayout() {
 
+        yellowView.snp.makeConstraints { make in
+            make.leading.equalTo(view.snp.leading).offset(30)
+            make.top.equalTo(view.snp.top).offset(30)
+            make.trailing.equalTo(view.snp.trailing).offset(-30)
+            make.bottom.equalTo(view.snp.bottom).offset(-30)
+        }
+        
+        
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        
+        orangeView.snp.makeConstraints { make in
+            make.top.equalTo(yellowView.snp.top).offset(50)
+            make.leading.equalTo(yellowView.snp.leading).offset(50)
+            make.trailing.equalTo(yellowView.snp.trailing).offset(-50)
+            make.height.equalTo(300)
+        }
+    }
+
+    
+    
 }
 
